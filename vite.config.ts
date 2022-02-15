018891b2ddd5272d119e162b49853f067a106348
在同一时空相遇 <y17870181601@163.com>
2022-02-15 11:57:37 +0800

    修改为中文消息提示，修改base

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue({
-    reactivityTransform: true
-  })]
-})
+  base: '/Game/vue-wordle/',
+  plugins: [
+    vue({
+      reactivityTransform: true,
+    }),
+  ],
+});
